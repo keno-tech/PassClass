57fc40b74150dc94b272330f51e9a2c651dccb5a
HZ <hyzhou@student.unimelb.edu.au>
2024-03-03 01:08:49 +1100

    Added ability to update data that was previously there based on filename

--- confluence.py
+++ confluence.py
@@ -14,25 +14,55 @@ headers = {
 
 
 def uploadTranscript(title, new_content):
-    data = {
-        'type': 'page',
-        'title': title,
-        'space': {'key': 'TEAM'},
-        'body': {
-            'storage': {
-                'value': new_content,
-                'representation': 'storage',
-            }
+    # Add check to see if the page exists
+    check_url = f'{confluence_base_url}/rest/api/content?title={title}&spaceKey=TEAM&expand=version'
+    check_response = requests.get(check_url, headers=headers, auth=auth)
+    check_data = check_response.json()['results'][0]
+    page_exists = False
+    if check_response.json()['size'] > 0:
+        page_exists = True
+
+    if page_exists:
+        # Update the data, increase version number
+        page_id = check_data['id']
+        current_version = check_data['version']['number']
+        new_url = f'{confluence_base_url}/rest/api/content/{page_id}'
+        data = {
+            'type': 'page',
+            'title': title,
+            'space': {'key': 'TEAM'},
+            'body': {
+                'storage': {
+                    'value': new_content,
+                    'representation': 'storage',
+                }
+            },
+            'version': {'number': current_version + 1}
         }
-    }
-    
-    create_url = f'{confluence_base_url}/rest/api/content/'
-    response = requests.post(create_url, data=json.dumps(data), headers=headers, auth=auth)
+        response = requests.put(new_url, data=json.dumps(data), headers=headers, auth=auth)
+    else:
+        # Simply create data without version number
+        data = {
+            'type': 'page',
+            'title': title,
+            'space': {'key': 'TEAM'},
+            'body': {
+                'storage': {
+                    'value': new_content,
+                    'representation': 'storage',
+                }
+            },
+        }
+        create_url = f'{confluence_base_url}/rest/api/content/'
+        response = requests.post(create_url, data=json.dumps(data), headers=headers, auth=auth)
+        page_id = response.json().get('id', 'N/A')
     
     if response.status_code == 200:
         print("Page created successfully.")
         page_id = response.json()['id']
         print(f"Page URL: {confluence_base_url}/spaces/TEAM/pages/{page_id}")
+    elif response.status_code == 204:
+        print("Page updated successfully")
     else:
         print("Failed to create page:", response.json())
 
